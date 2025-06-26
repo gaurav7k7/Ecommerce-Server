39500bfcdde028762870ba02f9228574a2903318
gaurav7k7 <gaurav798240@gmail.com>
2025-06-26 13:50:02 +0530

    server.js modified in order to resolve mongodb issue

--- server.js
+++ server.js
@@ -39,15 +39,8 @@ app.use("/api/address", addressRouter);
 
 const startServer = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log("MongoDB connected");
-
-    app.listen(process.env.PORT || 5000, () => {
-      console.log("Server is running");
-    });
   } catch (err) {
     console.error("MongoDB connection failed:", err);
     process.exit(1);
